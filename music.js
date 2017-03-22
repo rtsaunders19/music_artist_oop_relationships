6dfce78a0ba0c2f77c50c642b4d7c3dab6670dd8
Robert Saunders <rtsaunders19@gmail.com>
2017-03-22 00:40:24 -0400

    try

--- music.js
+++ music.js
@@ -1,17 +1,27 @@
-// Define the Artist constructor function here, with a 'name' property that can be set at instantiation,
-// and a 'songs' property that is an empty array to start
-function Artist() {
-
+function Artist(name) {
+  this.name = name;
+  this.songs = [];
 }
 
-// Define the Song constructor function here, with 'title' and 'artist' properties that can be set at instantiation,
-// and a 'playCount' property that is set to 0
-function Song() {
-
-  // When a new song is instantiated, you'll want to add that song to its artist's 'songs' array.
-  // You'll probably want to do that in here somewhere...
+Artist.prototype.addSong = function(song) {
+  this.songs.push(song);
 }
 
-// This is how we should be able to use your constructor functions once they work!
+function Song(title, artist) {
+  this.title = title || 'untitled';
+  this.artist = artist;
+  this.playCount = 0;
+}
+
+
 var vanHalen = new Artist("Van Halen");
 var jump = new Song("Jump!", vanHalen);
+var beatIt = new Song("Beat It", vanHalen);
+var theBeatles = new Artist("The Beatles");
+var loveMeDo = new Song("Love Me Do", theBeatles);
+console.log(vanHalen);
+console.log(theBeatles);
+
+
+vanHalen.addSong(jump);
+theBeatles.addSong(loveMeDo);
